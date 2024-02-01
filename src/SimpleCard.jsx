4cc7097dd95b9343bf881d99dfc126f6a221816d
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-01 14:24:48 +0000

    Card Format and styling completed

--- src/SimpleCard.jsx
+++ src/SimpleCard.jsx
@@ -4,10 +4,10 @@ function SimpleCard() {
     
     return (
         <div className="card" style={{width: "18rem"}}>
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <img src="..." className="card-img-top" style={{width: "500px", height:"325px"}} alt="..." />
+            <div className="card-body text-center">
+                <h5 className="card-title ">Card title</h5>
+                <p className="card-text ">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
       </div>
