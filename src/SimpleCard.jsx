6ce885fd6bb2809ee6172cbc66cb6e40dfa6c0cb
Michaelmira <151809512+Michaelmira@users.noreply.github.com>
2024-02-01 13:36:41 +0000

    Finish styling on navbar

--- src/SimpleCard.jsx
+++ src/SimpleCard.jsx
@@ -3,13 +3,14 @@ import React from "react";
 function SimpleCard() {
     
     return (
-        <div className="counter-display">           
-            <div className="card">
-                <h1>Simple Card</h1>
+        <div className="card" style={{width: "18rem"}}>
+            <img src="..." className="card-img-top" alt="..." />
+            <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
             </div>
-            
-
-        </div>
+      </div>
     );
 };
 
