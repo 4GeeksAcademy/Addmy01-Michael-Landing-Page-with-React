4569c62bd9366257caa47078c14a210327e73fd2
addmy01 <70408689+addmy01@users.noreply.github.com>
2024-02-01 01:03:50 +0000

    create simple card

---
+++ src/SimpleCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SimpleCard() {
+    
+    return (
+        <div className="counter-display">           
+            <div className="seconds ones">
+                <h1>Simple Card</h1>
+            </div>
+            
+
+        </div>
+    );
+};
+
+export default SimpleCard
