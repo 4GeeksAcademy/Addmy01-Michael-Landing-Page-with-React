3315414d68d9f3a309854042d18ae6b03c65d314
addmy01 <70408689+addmy01@users.noreply.github.com>
2024-02-01 01:39:37 +0000

    start working on navbar

---
+++ src/SimpleFooter.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function SimpleFooter() {
+    
+    return (
+        <div className="counter-display">           
+            <div className="footer">
+                <h1>Nav bar</h1>
+            </div>
+            
+
+        </div>
+    );
+};
+
+export default SimpleFooter
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import SimpleCard from "../../SimpleCard";
 import SimpleNavbar from "../../SimpleNavbar";
 import SimpleJumbotron from "../../SimpleJumbotron";
+import SimpleFooter from "../../SimpleFooter";
 
 //include images into your bundle
 
@@ -9,12 +10,17 @@ import SimpleJumbotron from "../../SimpleJumbotron";
 //create your first component
 const Home = () => {
 	return (
-	<div className="text-center">
-		<SimpleCard />
-		<SimpleJumbotron />
-
-		<SimpleNavbar />
-	</div>
+	<><div className="text-center">
+			<SimpleNavbar />
+		</div>
+		<div className="container">
+				<SimpleJumbotron />
+				<SimpleCard />
+			</div>
+		<div className="text-center">
+			<SimpleFooter />
+		</div>
+	</>
 	);
 };
 
